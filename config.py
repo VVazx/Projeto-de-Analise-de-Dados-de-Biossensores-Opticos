1b94fe6f03c5141456fb66a949b1a5e0f6730f9c
VVazx <victor.vaz@estudante.ufjf.br>
2025-11-07 18:02:25 -0300

    alteração de comenários

--- config.py
+++ config.py
@@ -10,11 +10,12 @@ antibodies_path = "C:/Users/Victor/OneDrive/Área de Trabalho/IL1_04-11-25/04_11
 # Exemplo: "IL1", "IL10", etc.
 sample_prefix = "IL1"
 # Configuração do filtro de espectro
+#por enquanto o único filtro funcional é o savitzky
 filter_config = {
     "type": "savitzky", # Tipo de filtro: "savitzky", "media_movel", "mediana",...
     "params": {
         "window": 53, # Esse é o tamanho da janela para o filtro escolhido, quanto maior mais suave fica o sinal
-        "order": 2 # Ordem do polinômio para o filtro escolhido, quanto maior mais próximo do sinal original fica o sinal
+        "order": 2 # Ordem do polinômio para o filtro escolhido, quanto maior o mais próximo o sinal fica do original
     }
 }
 
